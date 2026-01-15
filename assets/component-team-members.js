e983cf5d593481d5fc7b46302ce4abb61dd84a8c
chrishughs <chrisshughs@gmail.com>
2026-01-15 20:34:08 +0900

    add pop-up modal for about us team members

---
+++ assets/component-team-members.js
@@ -0,0 +1,64 @@
+class TeamMembers extends HTMLElement {
+  constructor() {
+    super()
+
+    this.triggers.forEach(trigger => {
+      trigger.addEventListener('click', this.modalOpen.bind(this))
+    })
+
+    this.closeTriggers.forEach(trigger => {
+      trigger.addEventListener('click', this.modalClose.bind(this))
+    })
+
+    this.addEventListener('keyup', (evt) => {
+      if (evt.code === 'Escape') {
+        this.modals.forEach(modal => {
+          modal?.classList.remove('active')
+        })
+        this.onModalClose()
+      }
+    })
+  }
+
+  modalOpen (e) {
+    const modalId = e.currentTarget?.getAttribute('data-index')
+    this.modals.forEach(modal => {
+      if (modal.getAttribute('data-index') == modalId) {
+        modal.classList.add('active')
+      } else {
+        modal.classList.remove('active')
+      }
+    })
+    this.onModalOpen()
+  }
+
+  modalClose (e) {
+    const modal = e.target.closest('.js-modal')
+    modal?.classList.remove('active')
+    this.onModalClose()
+  }
+
+  onModalOpen () {
+    document.body.classList.add('overflow-hidden');
+    document.querySelector('.header-wrapper').style.display = 'none';
+  }
+
+  onModalClose () {
+    document.body.classList.remove('overflow-hidden');
+    document.querySelector('.header-wrapper').style.display = '';
+  }
+
+  get triggers () {
+    return this.querySelectorAll('.js-modal-trigger')
+  }
+
+  get closeTriggers () {
+    return this.querySelectorAll('.js-modal-close')
+  }
+
+  get modals () {
+    return this.querySelectorAll('.js-modal')
+  }
+}
+
+customElements.define('team-members', TeamMembers)
